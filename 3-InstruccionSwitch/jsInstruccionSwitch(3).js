14c51221ff3c9c7723d1afcb085fa72d0ea6e2e7
Juan Cruz <31910958+juancruzjcm@users.noreply.github.com>
2019-07-15 12:03:32 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -4,6 +4,22 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
+
+switch(mesDelAño){
+    case "Febrero":
+        alert("Este mes no tiene mas de 29 dias")
+        break;
+    //case "Febrero":
+    default:
+        alert("Este mes tiene 30 dias o mas")
+
+
+
+
+
+
+}
+
 	
 	
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,7 +4,32 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+	switch(mesDelAño){
+        case "Febrero":
+        
+            alert("Este mes tiene 28 Días");
+            break;
+
+        case"Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+            alert("Este mes tiene 30 días");
+            break;
+
+        case "Mayo":
+        case "Marzo":
+        case "Julio":
+        case "Agosto":
+        case "Octubre":
+        case "Diciembre":
+        case "Enero":
+            alert("Este mes tiene 31 días");
+
+            break;
+
+
+    }
 	
 
 
